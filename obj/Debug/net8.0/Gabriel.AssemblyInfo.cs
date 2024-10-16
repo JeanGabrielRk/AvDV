fcef573635f86a053789f7f9eb373c6049448cfa
Auto Logon <autologon@CSED.LAB>
2024-10-16 11:25:48 -0300

    Versao final

--- obj/Debug/net8.0/Gabriel.AssemblyInfo.cs
+++ obj/Debug/net8.0/Gabriel.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gabriel")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0921b0719d1d19121fdac57aef077bf492c660ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73c261662df9c823057a2528e1a89d8e173b32e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gabriel")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gabriel")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
